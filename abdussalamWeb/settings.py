c83e6c445e6dadd8a0774c17aa0fbc4ee319b23a
abdussalam02 <samaniabdussalam02@gmail.com>
2022-02-08 18:17:21 +0530

    staticdir

--- abdussalamWeb/settings.py
+++ abdussalamWeb/settings.py
@@ -9,6 +9,9 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+
+import os
+from distutils import core
 import django_heroku
 from pathlib import Path
 
@@ -117,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
