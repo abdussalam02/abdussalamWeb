605b1a98d1d9e484974ee3ce04d12e9ef986e643
abdussalam02 <samaniabdussalam02@gmail.com>
2022-02-08 19:46:51 +0530

    changed debug from false to true

--- abdussalamWeb/settings.py
+++ abdussalamWeb/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4w@)tjcoz%s!9*5eahi=gep!+uupxy_xj4-3q&8v4e6!%f4iy3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'abdussalam.herokuapp.com']
 
